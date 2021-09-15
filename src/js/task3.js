853c96356ac8900386aafcc51b517d4f43e6bd1d
RomanIlienko <75617889+RomanIlienko@users.noreply.github.com>
2021-09-15 11:12:11 +0300

    fixed

--- src/js/task3.js
+++ src/js/task3.js
@@ -20,8 +20,8 @@ const makeTransaction = transaction => {
   })
 };
 
-const logSuccess = (id, time) => {
-  console.log(`Transaction ${id} processed in ${time}ms`);
+const logSuccess = ([id, time]) => {
+  console.log(`Transaction ${id} processed in ${time} ms`);
 };
 
 const logError = id => {
